974e629570e584fdf7039c9484a7d43e21fc15fe
ORavid25 <oravid12@gmail.com>
2022-02-07 20:31:37 +0200

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
+import "./style.css";
+import Logo from "./assats/logo.jpg";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <div className="main-wrapper">
+        <div className="header-container"></div>
+        <div className="main-container">
+          <div className="logo-container">
+              <img src={Logo} width="800"/>
+          </div>
+          <div className="info-container">
+          <h1 className="">Welcome to our social drawing App</h1>
+          <h2>Collaborative drawing board with rooms and chat abilities.</h2>
+          <p>invite friends and family and start the fun!</p>
+
+          <button type="button" className="startNowBtn">Start Now!</button>
+
+          </div>
+
+        </div>
+        <div className="container">
+
+        </div>
+      </div>
   );
 }
 
