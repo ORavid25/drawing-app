3939a7a3413870d917dacc566d0632c53278783c
Shaked5 <shakedshervi@gmail.com>
2022-02-09 14:22:05 +0200

    finish roomDisplay UI

--- src/components/RoomsDisplay.js
+++ src/components/RoomsDisplay.js
@@ -1,15 +1,16 @@
 import React from "react";
 import "../css/RoomsDisplay.css";
+import {FcLock,FcUnlock} from 'react-icons/fc';
 
 const RoomCard = () => {
   return (
     <div dir="rtl" className="card-container">
       <div className="roomName-container">
-        <p>Room 1</p>
+        <h2>Room 1</h2>
       </div>
       <div>
         {/* icon lock/unlocked */}
-        <p>Unlocked</p>
+        <h3>Unlocked <FcUnlock size={30}/></h3>
       </div>
     </div>
   );
