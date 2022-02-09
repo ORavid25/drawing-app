765e6d8d5c07bbb651bbcf7bb3e45cfe76a5bafe
Shaked5 <shakedshervi@gmail.com>
2022-02-09 15:40:31 +0200

    create canvasPage & add router

---
+++ src/pages/canvasPage.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import '../css/CanvasPage.css';
+
+const canvasPage = () => {
+    return (
+        <div className="container">
+            <div className="components-wrapper">
+                <div className="canvas">
+                    <h1>canvas</h1>
+                </div>
+                <div className="chat">
+                <h1>chat</h1>
+                </div>
+            </div>
+
+        </div>
+    );
+};
+
+export default canvasPage;
--- src/pages/routerManager.js
+++ src/pages/routerManager.js
@@ -1,5 +1,6 @@
 import React from "react";
 import {  Route  , Routes} from "react-router-dom";
+import CanvasPage from "./canvasPage";
 import HomePage from "./homePage";
 import RoomsPage from "./RoomsPage";
 
@@ -8,6 +9,7 @@ const routerManager = () => {
     <Routes>
       <Route path="/" element={<HomePage/>}/>
       <Route path="/rooms" element={<RoomsPage/>}/>
+      <Route path="/canvasRoom" element={<CanvasPage/>}/>
     
     </Routes>
     
